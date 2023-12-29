7c2dd8242fde3bc5501c95657845c912477764ff
priyankgit11 <priyankdave@weybee.in>
2023-12-29 20:10:07 +0530

    Completed Party List,Add Page

--- EvaluationPartyProduct/obj/Debug/net8.0/EvaluationPartyProduct.AssemblyInfo.cs
+++ EvaluationPartyProduct/obj/Debug/net8.0/EvaluationPartyProduct.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EvaluationPartyProduct")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0044a533a56d86b29887d3f55750e5f463471975")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EvaluationPartyProduct")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EvaluationPartyProduct")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- EvaluationPartyProduct/wwwroot/Party/index.js
+++ EvaluationPartyProduct/wwwroot/Party/index.js
@@ -1,1 +1,14 @@
 console.log("hello");
+
+//////////////FUNCTIONS
+
+function showToastMessage(message) {
+  const toastMessage = document.getElementById("toastMessage");
+  toastMessage.textContent = message;
+  toastMessage.className = "show";
+
+  // Hide toast message after 3 seconds
+  setTimeout(() => {
+    toastMessage.className = toastMessage.className.replace("show", "");
+  }, 3000);
+}
